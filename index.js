a83f780b9276906239b911d8456d9a2163ab4f28
RKRroushan <roushanroybitp22@gmail.com>
2018-05-27 01:47:12 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function validateTextbox() {
+ 
+var box = document.getElementById("name");
+
+var box = document.getElementById("");
+
+
+var box = document.getElementById("");
+
+if (box.value == "" || box.value == "" ){
+
+alert("The field name must contains a value");
+return false;
+ }
+ 
+ }
